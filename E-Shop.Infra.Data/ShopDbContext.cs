37a29869f973e7ec23d5b4ae717bc359109bff29
NaderWD <Nader.webdevop@gmail.com>
2025-02-03 12:45:07 +0330

    ShopDBContext Modified

--- E-Shop.Infra.Data/ShopDbContext.cs
+++ E-Shop.Infra.Data/ShopDbContext.cs
@@ -1,27 +1,43 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using E_Shop.Domain.Models;
+﻿using E_Shop.Domain.Models;
+using E_Shop.Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace E_Shop.Infra.Data
 {
-    public class ShopDbContext : DbContext
+    public class ShopDbContext(DbContextOptions<ShopDbContext> options) : DbContext(options)
     {
-        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
-        {
-
-        }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Product> Products { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Discount> Discounts { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ShopDbContext).Assembly);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseModel>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+            }
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseModel>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
     }
 }
