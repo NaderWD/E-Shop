7acc92308eb8d0b4a70e5a7561a44443e0e90937
NaderWD <Nader.webdevop@gmail.com>
2025-02-13 15:54:15 +0330

    Before Moving The Account To User Area

---
+++ E-Shop.Web/Areas/User/Controllers/UserBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop.Web.Areas.User.Controllers
+{
+    public class UserBaseController : Controller
+    {
+        protected string SuccessMessage = "SuccessMessage";
+        protected string ErrorMessage = "ErrorMessage";
+    }
+}
