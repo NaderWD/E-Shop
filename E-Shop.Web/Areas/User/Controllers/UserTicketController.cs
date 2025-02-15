ae7997b19962ed9377c240b464aeedf6077e6777
NaderWD <Nader.webdevop@gmail.com>
2025-02-15 19:54:14 +0330

    Ticket View Index And Create Added

--- E-Shop.Web/Areas/User/Controllers/UserTicketController.cs
+++ E-Shop.Web/Areas/User/Controllers/UserTicketController.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Shop.Application.Services.Interfaces;
+using E_Shop.Application.ViewModels.TicketViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace E_Shop.Web.Areas.User.Controllers
 {
-    public class UserTicketController : UserBaseController    
+    public class UserTicketController(ITicketService _service) : UserBaseController
     {
-        public IActionResult Index()     
+        [Route("UserTickets")]
+        public async Task<IActionResult> Index()
         {
+            await _service.GetAllTickets();
+            return View();
+        }
+
+        [HttpGet("CreateTicket")]
+        public IActionResult CreateTicket()
+        {
+            return View();
+        }
+
+        [HttpPost("CreateTicket")]
+        public async Task<IActionResult> CreateTicket(CreateTicketVM ticketVM)
+        {
+            await _service.CreateTicket(ticketVM);
+            return View();
+        }
+
+        [HttpGet("TicketMessages")]
+        public IActionResult TicketMessages()
+        {
+            return View();
+        }
+
+        [HttpPost("TicketMessages")]
+        public IActionResult TicketMessages(int ticketId)
+        {
+
             return View();
         }
     }
