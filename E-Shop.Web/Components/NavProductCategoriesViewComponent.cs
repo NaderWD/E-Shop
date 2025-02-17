b05d68948b6d832ff89446533465a28d8434f6fa
Matin-Rmz <Matinrmz82@gmail.com>
2025-02-17 16:40:41 +0330

    NavProductCategoriesModified in a component

---
+++ E-Shop.Web/Components/NavProductCategoriesViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using E_Shop.Application.Services.Implementations;
+using E_Shop.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop.Web.Components
+{
+    public class NavProductCategoriesViewComponent(IProductCategoriesService productCategoriesService) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var content = productCategoriesService.GetAll();
+            return View("GetNavProductCategories", content);
+        }
+    }
+}
