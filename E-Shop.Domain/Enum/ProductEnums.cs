0d57d9bd3758abec58552c88e341c8f2042a7b62
Matin-Rmz <Matinrmz82@gmail.com>
2025-03-02 17:25:39 +0330

    .

--- E-Shop.Domain/Enum/ProductEnums.cs
+++ E-Shop.Domain/Enum/ProductEnums.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace E_Shop.Domain.Enum
 {
-    puclic enum ProductEnums
+    public enum ProductEnums
+    {
+        [Display(Name ="")]
+        All,
+
+        [Display(Name = "")]
+        MostExpensive,
+
+        [Display(Name = "")]
+        MostRecent,
+    }
+    public enum SortOrder 
     {
 
     }
