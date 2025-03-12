6eec4c2d8fd243fef409a973307f45aa16c4677e
NaderWD <Nader.webdevop@gmail.com>
2025-03-12 18:41:44 +0330

    Address Classes Complete

--- E-Shop.Domain/Models/AddressModels/Address.cs
+++ E-Shop.Domain/Models/AddressModels/Address.cs
@@ -1,9 +1,9 @@
 ﻿namespace E_Shop.Domain.Models.AddressModels
 {
-    public class Address
+    public class Address : BaseModel
     {
-        public int HouseNumber { get; set; }         
-        public string StreetName { get; set; }            
+        public int HouseNumber { get; set; }
+        public string StreetName { get; set; }
         public string? NeighborHood { get; set; }
         public City? City { get; set; }
         public State? State { get; set; }
