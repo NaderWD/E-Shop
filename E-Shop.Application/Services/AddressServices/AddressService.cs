ca29fd228fe91214cc0879c88867131262f26734
NaderWD <Nader.webdevop@gmail.com>
2025-03-12 19:44:38 +0330

    Error Handled

--- E-Shop.Application/Services/AddressServices/AddressService.cs
+++ E-Shop.Application/Services/AddressServices/AddressService.cs
@@ -1,12 +1,21 @@
-﻿using E_Shop.Domain.Contracts.AddressCont;
+﻿using E_Shop.Application.ViewModels.AddressViewModels;
+using E_Shop.Domain.Contracts.AddressCont;
 
 namespace E_Shop.Application.Services.AddressServices
 {
-    public class AddressService(IStateRepository _stateRepository, 
-                                                 ICityRepository _cityRepository, 
-                                                 IAddressRepository _addressRepository, 
+    public class AddressService(IStateRepository _stateRepository,
+                                                 ICityRepository _cityRepository,
+                                                 IAddressRepository _addressRepository,
                                                  IUserAddressRepository _userAddressRepository) : IAddressService
     {
+        public Task CreateAddress(AddressVM addressVM)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<List<AddressVM>> GetUserAddresses(int userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
